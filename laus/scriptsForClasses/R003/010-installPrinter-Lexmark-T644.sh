cd01283cce8deac6e60e75e32baa0c1550b1ba40
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2014-10-21 11:31:39 +0200

    corrected IP for Printers in new R003 and R108

--- laus/scriptsForClasses/R003/010-installPrinter-Lexmark-T644.sh
+++ laus/scriptsForClasses/R003/010-installPrinter-Lexmark-T644.sh
@@ -18,5 +18,5 @@ fi
 
 #	Note the two -E options. The first one (before -p) forces encryption when connecting to the server. The last one enables the destination and starts accepting jobs.
 
-lpadmin -E -p Raum-001-Printer -v socket://10.0.1.91 -m 'foomatic-db-compressed-ppds:0/ppd/foomatic-ppd/Lexmark-T644-Postscript.ppd' -L "Drucker im Raum-001" -E
+lpadmin -E -p Raum-001-Printer -v socket://10.0.3.91 -m 'foomatic-db-compressed-ppds:0/ppd/foomatic-ppd/Lexmark-T644-Postscript.ppd' -L "Drucker im Raum-001" -E
 
