98333d9837385d08abac10f7fb27eb8181cdfaab
Reinhard Fink <reinhard.fink@tsn.at>
2014-12-20 19:03:46 +0100

    clean up laus-server.sh

--- laus/laus-server.sh
+++ laus/laus-server.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 #
+## source system - functions
+. /lib/init/vars.sh
+. /lib/lsb/init-functions
+
 # Shell Variables sourced from 
 # "LAUS calling Client"/etc/default/laus-setup
-#      
-#
+. /etc/default/laus-setup
+
 ## ENABLE_AUTOUPDATE="yes"
 # Server hosting all LAUS - scripts:
 ## LAUS_SERVER="laus01"
@@ -16,23 +20,21 @@
 # Path, where updatelogfiles are written to:
 ## UPDATE_LOG_DIR="/var/log/laus"
 
-
-######################################################################################
-################## S T A R T   O F   S C R I P T #####################################
-######################################################################################
-
 # Updatescripts may not be executed on LAUS-SERVER!
-# replace "laus01" with hostname of our LAUS-Server
-if test $(hostname) = "laus01" 
+if test $(hostname) = ${LAUS_SERVER}
 then
 	log_action_begin_msg "Updatescript may not run on  laus - Server!"
 	echo "Updatescript may not run on  laus - Server!"
 	exit 7
 fi
 
+######################################################################################
+################## Helper Function ###################################################
+######################################################################################
+
 # Function for executing all Scripts in Directory
 function executeScripts {
-	if test -z $1;
+	if [ -z $1 ];
 	then
 		classHirarchy="ALLCLASSES";
 	else
@@ -40,41 +42,33 @@ function executeScripts {
 	fi
 
 	fileList=$(ls)
-	for file in $fileList; do
-		# test, if file is executable
-		# echo $file
-		if test -f $file -a -x $file -a $file = ${file%"~"};
+	for file in ${fileList}; do
+		# test, if file is executable and does not end with ~
+		if [ -f ${file} -a -x ${file} -a ${file} = ${file%"~"} ];
 		then
 			# test, if script has already been executed on this workstation
-			if test -f $UPDATE_LOG_DIR$"/"$classHirarchy"."$file
+			if [ -f ${UPDATE_LOG_DIR}"/"${classHirarchy}"."${file} ]
 			then
-				log_action_begin_msg "already executed --> "$classHirarchy"."$file
-				#echo "already executed --> "$classHirarchy"."$file
+				log_action_begin_msg "already executed --> "${classHirarchy}"."${file}
+				#echo "already executed --> "${classHirarchy}"."${file}
 			else
-				log_action_begin_msg "running LAUS script --> "$classHirarchy"."$file
-				#echo "running LAUS script --> "$classHirarchy"."$file
-				"./"$file $startParameter
+				log_action_begin_msg "running LAUS script --> "${classHirarchy}"."${file}
+				#echo "running LAUS script --> "${classHirarchy}"."${file}
+				"./"${file} ${startParameter}
 				# if script has been executed, log it"
-				if test $? -eq 0;
+				if [ $? -eq 0 ];
 				then
-					cp $file $UPDATE_LOG_DIR$"/"$classHirarchy"."$file;
+					cp ${file} ${UPDATE_LOG_DIR}"/"${classHirarchy}"."${file}
 				fi
 			fi
 		fi
 	done
 }
 
-## source system - functions
-. /lib/init/vars.sh
-. /lib/lsb/init-functions
 
-# source Variables from laus-setup
-. /etc/default/laus-setup
-
-# set HOSTCLASS Variable from File hostsToClasses
-#HOSTCLASS=$(grep $(hostname) hostsToClasses | awk 'BEGIN { FS = ";" } { print $2 }')
-#SUBHOSTCLASS=$(grep $(hostname) hostsToClasses | awk 'BEGIN { FS = ";" } { print $3 }')
-#SUBSUBHOSTCLASS=$(grep $(hostname) hostsToClasses | awk 'BEGIN { FS = ";" } { print $4 }')
+######################################################################################
+################## S T A R T   O F   S C R I P T #####################################
+######################################################################################
 
 # set HOSTCLASS Variable from File hostsToClasses
 # check like tftp:
@@ -89,39 +83,32 @@ function executeScripts {
 # and collect all information in:
 # HOSTCLASS, SUBHOSTCLASS and SUBSUBHOSTCLASS
 #
-# see: HOSTCLASS=$HOSTCLASS" "$(grep ^$TESTSTRING";" hostsToClasses | awk 'BEGIN { FS = ";" } { print $2 }')
+# see: HOSTCLASS=${HOSTCLASS}" "$(grep ^${TESTSTRING}";" hostsToClasses | awk 'BEGIN { FS = ";" } { print $2 }')
 # and so on!
 #
 # hostsToClasses in format:
 # HOSTNAME;HOSTCLASS;SUBHOSTCLASS;SUSUBHOSTCLASS
 # r001pc50;TEACHER BEAMER;BEAMER_1024x768
 # r001;R001
-# => HOSTCLASS=TEACHER BEAMER R001 and SUBHOSTCLASS=BEAMER_1024x768
+# => HOSTCLASS for PC with hostname r001pc50: TEACHER BEAMER R001
+#    and SUBHOSTCLASS=BEAMER_1024x768
 #
 TESTSTRING=$(hostname)
-# echo $TESTSTRING
-# TESTSTRING_LENGTH=${#TESTSTRING}
 for ((length=${#TESTSTRING};  length > 0; length--)) 
 do
 	TESTSTRING=${TESTSTRING:0:length}
-	if grep ^$TESTSTRING";" hostsToClasses
+	if grep ^${TESTSTRING}";" hostsToClasses
 	then
-		HOSTCLASS=$HOSTCLASS" "$(grep ^$TESTSTRING";" hostsToClasses | awk 'BEGIN { FS = ";" } { print $2 }')
-		# echo "setzen von HOSTCLASS"
-		# echo $HOSTCLASS
-		SUBHOSTCLASS=$SUBHOSTCLASS" "$(grep ^$TESTSTRING";" hostsToClasses | awk 'BEGIN { FS = ";" } { print $3 }')
-		# echo "setzen von SUBHOSTCLASS"
-		# echo $SUBHOSTCLASS
-		SUBSUBHOSTCLASS=$SUBSUBHOSTCLASS" "$(grep ^$TESTSTRING";" hostsToClasses | awk 'BEGIN { FS = ";" } { print $4 }')
-		# echo "setzen von SUBSUBHOSTCLASS"
-		# echo $SUBSUBHOSTCLASS
+		HOSTCLASS=${HOSTCLASS}" "$(grep ^${TESTSTRING}";" hostsToClasses | awk 'BEGIN { FS = ";" } { print $2 }')
+		SUBHOSTCLASS=${SUBHOSTCLASS}" "$(grep ^${TESTSTRING}";" hostsToClasses | awk 'BEGIN { FS = ";" } { print $3 }')
+		SUBSUBHOSTCLASS=${SUBSUBHOSTCLASS}" "$(grep ^${TESTSTRING}";" hostsToClasses | awk 'BEGIN { FS = ";" } { print $4 }')
 	fi
 done
 
 # souround List of hosts with () to cast to array
-HOSTCLASS=($HOSTCLASS)
-SUBHOSTCLASS=($SUBHOSTCLASS)
-SUBSUBHOSTCLASS=($SUBSUBHOSTCLASS)
+HOSTCLASS=(${HOSTCLASS})
+SUBHOSTCLASS=(${SUBHOSTCLASS})
+SUBSUBHOSTCLASS=(${SUBSUBHOSTCLASS})
 
 log_action_begin_msg "LAUS START --------------------------------------"
 
@@ -135,20 +122,20 @@ executeScripts
 # run scripts for classes, sub- and subsubclasses
 
 for hostclass in ${HOSTCLASS[@]}; do
-	if test -d $hostclass; 
+	if test -d ${hostclass}; 
 	then
-		cd $hostclass;
-		executeScripts $hostclass;
+		cd ${hostclass};
+		executeScripts ${hostclass};
 		for subhostclass in ${SUBHOSTCLASS[@]}; do
-			if test -d $subhostclass; 
+			if test -d ${subhostclass}; 
 			then
-				cd $subhostclass;
-				executeScripts $hostclass"."$subhostclass;
+				cd ${subhostclass};
+				executeScripts ${hostclass}"."${subhostclass};
 				for subsubhostclass in ${SUBSUBHOSTCLASS[@]}; do
-					if test -d $subsubhostclass; 
+					if test -d ${subsubhostclass}; 
 					then
-						cd $subsubhostclass;
-						executeScripts $hostclass"."$subhostclass"."$subsubhostclass;
+						cd ${subsubhostclass};
+						executeScripts ${hostclass}"."${subhostclass}"."${subsubhostclass};
 						cd ..;
 					fi;
 				done;
