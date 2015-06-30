9779b33924e90e242311305dbefc2c7e6eaf3081
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2015-06-30 10:25:33 +0200

    repair Rights for Dropbox and uname in Google Earth

--- laus/scriptsForClasses/UBUNTU1404/080-installGoogleEarth.sh
+++ laus/scriptsForClasses/UBUNTU1404/080-installGoogleEarth.sh
@@ -17,7 +17,7 @@ apt-get -y update
 
 apt-get -y install lsb-core 
 
-if [ $(uname -m) == "x86_644" ];
+if [ $(uname -m) == "x86_64" ];
 then
 	apt-get -y install libc6:i386 libfontconfig1:i386 libglib2.0-0:i386 libglu1-mesa:i386 libsm6:i386 libx11-6:i386 libxext6:i386 libxrender1:i386 
 	pkg=$(dpkg-query -l libgl1-mesa-glx | grep -m1 ^ii[[:space:]]* | grep -o libgl1-mesa-glx[[:graph:]]* | sed -r s/:\(i386\|amd64\)/""/g)
