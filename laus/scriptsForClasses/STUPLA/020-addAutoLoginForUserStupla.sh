2623918708bbe71ee40d1d60cc8a23ceeecd0510
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2015-11-10 14:37:40 +0100

    update class STUPLA

--- laus/scriptsForClasses/STUPLA/020-addAutoLoginForUserStupla.sh
+++ laus/scriptsForClasses/STUPLA/020-addAutoLoginForUserStupla.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-file="/etc/lightdm/lightdm.conf"
+file="/etc/lightdm/lightdm.conf.d/40-autologin.conf"
 if ! test -f $file".original"; then
 	cp $file $file".original"
 fi
@@ -8,7 +8,11 @@ updatetime=$(date +%Y%m%d-%T)
 newfile=$file".laus."$updatetime
 cp $file $newfile
 
-echo "" >> /etc/lightdm/lightdm.conf
-echo "autologin-user=stupla" >> /etc/lightdm/lightdm.conf
+echo "[SeatDefaults]
+
+autologin-user=stupla
+
+autologin-user-timeout=0
+" >> $file
 
 service lightdm restart
--- laus/scriptsForClasses/STUPLA/files/README.txt
+++ laus/scriptsForClasses/STUPLA/files/README.txt
@@ -1,1 +1,4 @@
 chrome.desktop nach .config/autostart kopieren 
+
+Remote Zugriff freischalten:
+in Lens: Freigabe der Arbeitsfläche erlauben & Bestätigung abschalten
