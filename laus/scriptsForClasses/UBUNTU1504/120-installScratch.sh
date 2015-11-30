521b09861e763f18ed8043780d11b9f6c5fee053
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2015-11-30 15:03:19 +0100

    delete UBUNTU15.04

--- laus/scriptsForClasses/UBUNTU1504/120-installScratch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#apt-get -y update
-
-# Scratch
-apt-get -y install scratch
--- laus/scriptsForClasses/UBUNTU1504/files/restore-standard-user.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Restore Standard - User
-#
-# replace standard user configuration with saved version
-
-rm -R /home/user
-cp -R -p /home/user.save /home/user
-chown -R user:2000 /home/user
