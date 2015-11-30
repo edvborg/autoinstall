f03afca1d76c31af45997b035325938895cb3397
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2015-11-30 15:08:00 +0100

    add Virtualbox 5.0

--- laus/scriptsForClasses/UBUNTU1404/060-installVirtualisation.sh
+++ laus/scriptsForClasses/UBUNTU1404/060-installVirtualisation.sh
@@ -4,4 +4,20 @@
 apt-get -y install wine
 
 ## Virtualbox
-apt-get -y install virtualbox
+#broken with Virtualbox 4.34
+#apt-get -y install virtualbox
+
+
+# install Virtualbox 5.0
+
+# add key for repository 
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add -
+
+# add repository
+echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" > /etc/apt/sources.list.d/virtualbox.list
+
+apt-get update
+
+# install Virtualbox 5.0
+apt-get -y install virtualbox-5.0
+
