02c2f1dea9a7fb137ab8746292ada2a4dcfc4877
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2016-04-06 12:22:25 +0200

    added new links to makeDesktopLinkForTestees.sh

--- laus/scriptsForClasses/UBUNTU1404/files/makeDesktopLinkForTestees.sh
+++ laus/scriptsForClasses/UBUNTU1404/files/makeDesktopLinkForTestees.sh
@@ -9,14 +9,14 @@ then
 	ln -s /home/shares/schueler/Testees/Abgabe_$USER $HOME/Schreibtisch/Abgabe_$USER
 	chown $USER:2000 $HOME/Schreibtisch/Abgabe_$USER
 
+	ln -s /home/shares/schueler/Testees/Abgabe_$USER $HOME/Abgabe_$USER
+	chown $USER:2000 $HOME/Abgabe_$USER
+
 	ln -s /home/shares/schueler/Testees/Vorlagen/ $HOME/Schreibtisch/Vorlagen
 	chown $USER:2000 $HOME/Schreibtisch/Vorlagen
 
 	cp /opt/vms/win7.desktop $HOME/Schreibtisch/
 	chown $USER:2000 $HOME/Schreibtisch/win7.desktop
-
-	cp /home/shares/optProgs/GeoGebraExam/GeoGebraExam.desktop $HOME/Schreibtisch/
-	chown $USER:2000 $HOME/Schreibtisch/GeoGebraExam.desktop
 fi
 
 
