11209a1f85694cf8be187a5cc933ccf1caa54886
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2016-10-03 14:49:24 +0200

    libdvdread -> libdvd-pkg for DVDs

--- laus/scriptsForClasses/UBUNTU1604/130-installMultimedia.sh
+++ laus/scriptsForClasses/UBUNTU1604/130-installMultimedia.sh
@@ -6,9 +6,10 @@
 apt-get -y install libmp3lame0 libtunepim5-mp3 libk3b6-extracodecs libavodec-unstripped-52 libxine1-ffmpeg
 
 ## Stuff to play DVDs
-apt-get -y install libdvdread4
-# run script to make libdvdread4 working
-/usr/share/doc/libdvdread4/install-css.sh
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y install libdvd-pkg  
+# reconfigure to make libdvdread4 working
+dpkg-reconfigure libdvd-pkg 
 
 ## VLC - Player
 ## Audacity
