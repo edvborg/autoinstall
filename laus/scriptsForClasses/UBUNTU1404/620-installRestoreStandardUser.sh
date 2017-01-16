bd34d258cddaa84bd6076e49d734b590e493aae5
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2017-01-16 17:10:04 +0100

    changed installation & update for users.save to user.save.NN.NN -> user.save

--- laus/scriptsForClasses/UBUNTU1404/620-installRestoreStandardUser.sh
+++ laus/scriptsForClasses/UBUNTU1404/620-installRestoreStandardUser.sh
@@ -6,7 +6,7 @@ SOURCE_PATH=$MOUNT_PATH_ON_CLIENT/xBigFiles
 
 cp files/restore-standard-user.conf /etc/init/
 
-cp -p -R $SOURCE_PATH/user.save /home/
+cp -p -R $SOURCE_PATH/user.save.$(lsb_release -r -s) /home/user.save
 
 # delete ibus - drirectory, because old values change keyboard layout sometimes
 # http://askubuntu.com/questions/454646/keyboard-layout-en-after-boot
