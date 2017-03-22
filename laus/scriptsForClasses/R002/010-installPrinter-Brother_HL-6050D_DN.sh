48b3c8797dd332a778c14359c80360820c497901
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2017-03-22 14:42:47 +0100

    changed Printer in R002 back to Laserjet 4

--- laus/scriptsForClasses/R002/010-installPrinter-Brother_HL-6050D_DN.sh
+++ laus/scriptsForClasses/R002/010-installPrinter-Brother_HL-6050D_DN.sh
@@ -20,9 +20,12 @@ fi
 
 # Brother-HL-6050D_DN-Postscript.ppd slow with complex PDFs
 # lpadmin -E -p KonfZi-Printer -v socket://10.1.13.91 -m 'foomatic-db-compressed-ppds:0/ppd/foomatic-ppd/Brother-HL-6050D_DN-Postscript.ppd' -L "Drucker im Konferenzzimmer" -E
+# HL-6050 BR-Script3 slow with complex PDFs
+# lpadmin -E -p R002-Printer -v socket://r002pr01 -m 'openprinting-ppds:0/ppd/openprinting/Brother/BR6050D2_GPL.ppd' -L "Drucker im Raum 002" -E
+
 
 # HP Laserjet Emulation doesn't print plain text files
 # lpadmin -E -p R002-Printer -v socket://r002pr01 -m 'gutenprint.5.2://hp-lj_4_plus' -L "Drucker im Raum 002" -E
 
-lpadmin -E -p R002-Printer -v socket://r002pr01 -m 'openprinting-ppds:0/ppd/openprinting/Brother/BR6050D2_GPL.ppd' -L "Drucker im Raum 002" -E
+lpadmin -E -p R002-Printer -v socket://r002pr01 -m 'foomatic-db-compressed-ppds:0/ppd/foomatic-ppd/HP-LaserJet_4M-ljet4.ppd' -L "Drucker im Raum 002" -E
 
