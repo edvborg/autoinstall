72d9f252ca9f4a10b16d8b1bb86f699159b9dee5
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2017-04-24 15:43:36 +0200

    add owncloud-client & ubuntu-restricted-extras

--- laus/scriptsForClasses/UBUNTU1604/130-installMultimedia.sh
+++ laus/scriptsForClasses/UBUNTU1604/130-installMultimedia.sh
@@ -6,7 +6,7 @@
 export DEBIAN_FRONTEND=noninteractive
 
 ## some Multimedia - Codecs
-apt-get -y install libmp3lame0 libtunepim5-mp3 libk3b6-extracodecs libavodec-unstripped-52 libxine1-ffmpeg
+apt-get -y install ubuntu-restricted-extras libmp3lame0 libtunepim5-mp3 libk3b6-extracodecs libavodec-unstripped-52 libxine1-ffmpeg
 
 ## Stuff to play DVDs
 apt-get -y install libdvd-pkg  
