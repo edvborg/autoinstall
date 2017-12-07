6a316f33a77987600f36cc77d558652583808e18
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2017-12-07 09:14:47 +0100

    enable www on testee logout

--- laus/scriptsForClasses/APP1804/590-testeeBlockWWW.sh
+++ laus/scriptsForClasses/APP1804/590-testeeBlockWWW.sh
@@ -9,11 +9,19 @@ HOOKED_SCRIPT="blockWWWForTestees.sh"
 cp  files/$HOOKED_SCRIPT /usr/local/bin/
 chmod 750 /usr/local/bin/$HOOKED_SCRIPT
 
+# START to block WWW on login
 # Add Hook $HOOKED_SCRIPT to File /etc/gdm3/PreSession/Default
 echo "
 # Hook to /usr/local/bin/$HOOKED_SCRIPT
 /bin/bash /usr/local/bin/$HOOKED_SCRIPT start &
 " >> $CONF_DIR/$CONF_FILE
 
-
+# STOP to block WWW on logout
+# Add Hook $HOOKED_SCRIPT to File /etc/gdm3/PostSession/Default
+# Replace original /etc/gdm3/PostSession/Default
+CONF_DIR="/etc/gdm3/PostSession"
+echo "
+# Hook to /usr/local/bin/$HOOKED_SCRIPT
+/bin/bash /usr/local/bin/$HOOKED_SCRIPT stop &
+" > $CONF_DIR/$CONF_FILE
 
