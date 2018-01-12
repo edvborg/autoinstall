a86f575cb422ee51013efc5b3aa09210bf168b3e
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2018-01-12 15:27:13 +0100

    add check, if Testee Desktop Links already exist

--- laus/scriptsForClasses/APP1804/510-testeeMakeDesktopLink.sh
+++ laus/scriptsForClasses/APP1804/510-testeeMakeDesktopLink.sh
@@ -15,20 +15,8 @@ X-GNOME-Autostart-Phase=Applications
 NoDisplay=true
 " >> /etc/xdg/autostart/app-makeDesktopLinkForTestees.desktop
 
-# create
-# shell script : /usr/local/bin/makeDesktopLinkForTestees.sh
-# ATTENTION ' instead " because $USER shall NOT be interpreted
-echo '#!/bin/bash
-
-if [[ $USER == testee* ]];
-then
-	ln -s /home/shares/schueler/Testees/Abgabe_$USER $HOME/Schreibtisch/Abgabe_$USER
-	
-	ln -s /home/shares/schueler/Testees/Abgabe_$USER $HOME/Abgabe_$USER
-	
-	ln -s /home/shares/schueler/Testees/Vorlagen/ $HOME/Schreibtisch/Vorlagen
-fi
-' >> /usr/local/bin/makeDesktopLinkForTestees.sh
+# copy shell script to: /usr/local/bin/makeDesktopLinkForTestees.sh
+cp files/makeDesktopLinkForTestees.sh /usr/local/bin/makeDesktopLinkForTestees.sh
 
 chmod a+x /usr/local/bin/makeDesktopLinkForTestees.sh
 
