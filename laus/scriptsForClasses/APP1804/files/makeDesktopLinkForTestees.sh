c07beabef9a3e9a662070007bbde7a85e8d2f4c6
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2018-04-12 14:05:36 +0200

    Link Vorlagen for Testees in /home/teachers/ttfinr

--- laus/scriptsForClasses/APP1804/files/makeDesktopLinkForTestees.sh
+++ laus/scriptsForClasses/APP1804/files/makeDesktopLinkForTestees.sh
@@ -7,6 +7,11 @@ then
 		ln -s /home/shares/schueler/Testees/Abgabe_$USER $HOME/Abgabe_$USER
 	fi
 
+	if [ ! -d $HOME/Abgabe_Vorlagen ];
+	then
+		ln -s /home/shares/schueler/Testees/Vorlagen/ $HOME/Abgabe_Vorlagen
+	fi
+
 	if [ ! -d $HOME/Schreibtisch/Abgabe_$USER ];
 	then
 		ln -s /home/shares/schueler/Testees/Abgabe_$USER $HOME/Schreibtisch/Abgabe_$USER
