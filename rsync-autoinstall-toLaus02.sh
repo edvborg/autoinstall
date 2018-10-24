359799489b7797f07f8df03ff1835bb5cd9c78e5
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2018-10-24 16:54:28 +0200

    added rsync files to laus02

---
+++ rsync-autoinstall-toLaus02.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rsync --progress -a -u /opt/autoinstall/ root@laus02:/opt/autoinstall/
---
+++ rsync-xBigFiles-toLaus02.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rsync --progress -a -u /opt/autoinstall/xBigFiles/ root@laus02:/opt/autoinstall/xBigFiles/
