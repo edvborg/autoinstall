90d59bbf834dca2dc6aa7354ffc61b261a9f3886
Reinhard Fink @ Home <reinhard.fink@tsn.at>
2019-10-30 13:37:20 +0100

    merge 4 apt-get into 1, remove ununnecessary apt-get update

--- laus/scriptsForClasses/AGI1804/115-installCodecs.sh
+++ laus/scriptsForClasses/AGI1804/115-installCodecs.sh
@@ -3,8 +3,5 @@
 export DEBIAN_FRONTEND=noninteractive
 
 ## Multimedia-Codecs
-apt-get -y install libmp3lame0 libk3b6 libk3b6-extracodecs
-apt-get -y install libavcodec-extra libavcodec-ffmpeg-extra56
-apt-get -y install libxine2 libxine2-ffmpeg
-apt-get -y update
+apt-get -y install libmp3lame0 libk3b6 libk3b6-extracodecs libavcodec-extra libavcodec-ffmpeg-extra56 libxine2 libxine2-ffmpeg
 
