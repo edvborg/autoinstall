7db085e2b12a1442f1652f2565645650ce087c3a
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2020-02-28 17:46:31 +0100

    add gthumb imageviewer

--- laus/scriptsForClasses/INSTALL_U2004/120-installGraphics.sh
+++ laus/scriptsForClasses/INSTALL_U2004/120-installGraphics.sh
@@ -4,10 +4,11 @@
 export DEBIAN_FRONTEND=noninteractive
 
 ## Gimp: pixel grafic
+## gthumb: image viewer & simple manipulator
 ## Inkscape: vector drawing program
 ## Scribus: DTP
 ## Krita: creative drawing
 ## Blender: modelling & rendering software
 ## OpenScad: for creating solid 3D CAD objects
 ## RawTherapee: Editor for RAW fotos 
-apt-get -y install gimp inkscape scribus krita blender openscad rawtherapee
+apt-get -y install gimp gthumb inkscape scribus krita blender openscad rawtherapee
