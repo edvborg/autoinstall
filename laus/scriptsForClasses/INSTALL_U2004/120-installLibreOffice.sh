700ae0df613fe4d5fb1f3ac23f89ceb85ccee58e
Reinhard Fink @ Home <reinhard.fink@tsn.at>
2020-03-30 10:44:49 +0200

    add libreOffice java connection

--- laus/scriptsForClasses/INSTALL_U2004/120-installLibreOffice.sh
+++ laus/scriptsForClasses/INSTALL_U2004/120-installLibreOffice.sh
@@ -7,7 +7,8 @@ export DEBIAN_FRONTEND=noninteractive
 
 ## LibreOffice full Package
 ## LibreOffice German Languagepackage
+## LibreOffice java connection needed e.g. language-tool
 ## LibreOffice Englisch-GB, German-AT, France, Italian, Spain Dictionarys
 ## Hyphenation & Thesaurus
-apt-get -y install libreoffice libreoffice-l10n-de hunspell hunspell-de-at hunspell-en-gb hunspell-fr hunspell-it hunspell-es hyphen-de mythes-de
+apt-get -y install libreoffice libreoffice-l10n-de libreoffice-java-common hunspell hunspell-de-at hunspell-en-gb hunspell-fr hunspell-it hunspell-es hyphen-de mythes-de
 
