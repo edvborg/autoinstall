86b8455f12667ff264d8e651b75e6814d0ad0b1d
root <root@laus01.app.tsn>
2020-05-13 20:16:33 +0000

    fix printer-names in lpoptions

--- laus/scriptsForClasses/APP/R115/020-set-lpoptions-CanoniR-ADV_C7260.sh
+++ laus/scriptsForClasses/APP/R115/020-set-lpoptions-CanoniR-ADV_C7260.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-PRINTER_NAME_SW="securePrint_sw"
-PRINTER_NAME_COLOR="securePrint_color"
-
-echo "Dest securePrint_color ColourModel=Colour Duplex=None
-Default securePrint_sw Duplex=None
+echo "Dest KonfZi-Kopierer_color ColourModel=Colour Duplex=None
+Default KonfZi-Kopierer_sw Duplex=None
 " > /etc/cups/lpoptions
 
 chown root:lp /etc/cups/lpoptions
--- laus/scriptsForClasses/APP/UNIFLOW/020-set-lpoptions-CanoniR-ADV_C7260.sh
+++ laus/scriptsForClasses/APP/UNIFLOW/020-set-lpoptions-CanoniR-ADV_C7260.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-PRINTER_NAME_SW="securePrint_sw"
-PRINTER_NAME_COLOR="securePrint_color"
-
 echo "Dest securePrint_color ColourModel=Colour Duplex=None
 Default securePrint_sw Duplex=None
 " > /etc/cups/lpoptions
