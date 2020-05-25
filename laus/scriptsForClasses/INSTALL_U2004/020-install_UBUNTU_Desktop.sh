23ac507ff2f59e3c6549935bf897d5a15ed9057a
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2020-05-25 16:33:37 +0200

    added gnome-shell-extensions

--- laus/scriptsForClasses/INSTALL_U2004/020-install_UBUNTU_Desktop.sh
+++ laus/scriptsForClasses/INSTALL_U2004/020-install_UBUNTU_Desktop.sh
@@ -7,8 +7,9 @@ export DEBIAN_FRONTEND=noninteractive
 
 ## Full Ubuntu - GNOME - Desktop
 ## dconf-editor: editor to customize hidden desktop options
+## gnome-shell-extension: program to control shell extensions
 ## compizconfig-settings-manager: more Settings for Compiz Graphic Manager
 ## chrome-gnome-shell: backend to install gnome-extensions via browser
 ## firefox-locale-de: german language pack firefox
 ## mc: Midnight commander
-apt-get -y install ubuntu-desktop dconf-editor compizconfig-settings-manager chrome-gnome-shell firefox-locale-de mc
+apt-get -y install ubuntu-desktop dconf-editor gnome-shell-extensions compizconfig-settings-manager chrome-gnome-shell firefox-locale-de mc
