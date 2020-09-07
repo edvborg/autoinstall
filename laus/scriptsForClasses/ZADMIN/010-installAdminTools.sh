2d72d4a21709a56fee0f14f541057ef88850b4eb
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2020-09-07 17:25:37 +0200

    added group ZADMIN for Admin PCs

---
+++ laus/scriptsForClasses/ZADMIN/010-installAdminTools.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# quiet installation
+export DEBIAN_FRONTEND=noninteractive
+
+## ldap-utils
+## jxplorer: Java LDAP Explorer
+## wakeonlan
+apt-get -y install ldap-utils jxplorer wakeonlan
+
